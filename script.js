26d69e1b94fbc27d82146ac1d118c047208f5529
Kunwar Randhir Singh <62481165+kunwarrohit@users.noreply.github.com>
2022-09-16 19:48:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,101 +1,98 @@
 'use strict';
+//Document object model: Structure Representation of HTML document. 
+// Allows Javascript to access HTML element and styles to manipulate them
 
-let secretNumber=Math.trunc( Math.random() * 20 ) + 1;
+//Change text,html attribute and even css style.
 
+/*
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent='Correct Number!';
+
+document.querySelector('.number').textContent=13;
+document.querySelector('.score').textContent=20;
+
+document.querySelector('.guess').value=0;
+console.log(document.querySelector('.guess').value);
+*/
+
+let secretNumber = Math.trunc(Math.random()*20)+1;
 let score=20;
-let highscore=0;
+let highscore = 0;
 
 const displayMessage = function(message){
     document.querySelector('.message').textContent=message;
+};
+const displayScore = function(score){
+    document.querySelector('.score').textContent=score;
 }
 
-document.querySelector('.check').addEventListener('click',
-function(){
+document.querySelector('.check').addEventListener('click',function(){
     const guess=Number(document.querySelector('.guess').value);
-    console.log(guess,typeof guess);
+    console.log(guess);
+    console.log(guess, typeof guess);
 
-    //when there is no input
-    if(!guess){
-        document.querySelector('.message').textContent='No Number!'
-        displayMessage('No Number!');
-    }
-    //when player wins
-    else if(guess === secretNumber){
-        // document.querySelector('.message').textContent='Correct Number!👍';
-        displayMessage('Correct Number!👍');
-
-        document.querySelector('.number').textContent=secretNumber;
-        score++;
-        document.querySelector('.score').textContent=score;
-
-        document.querySelector('body').style.backgroundColor='#60b347';
-        
-        document.querySelector('.number').style.width='30rem';
-
-        if(score>highscore){
-            highscore=score;
-            document.querySelector('.highscore').textContent=highscore;            
-        }
-        
-    }
-    //when guess is wrong
-    else if(quess!==secretNumber){
-        if(score >1){
-            // document.querySelector('.message').textContent=guess > secretNumber ? '📉Too Low!':'📈Too High!';
-            displayMessage(guess > secretNumber ? '📉Too High!':'📈Too Low!');
-
+//for Emojis: Window + .
+    //when guess is 0
+    if(guess==0){
+        if(score>1){
+            displayMessage('😒No Number! "ZERO"');
             score--;
-            document.querySelector('.score').textContent=score;
+            displayScore(score);
         }
         else{
-            // document.querySelector('.message').textContent='😬You Lost the Game'
-            displayMessage('😬You Lost the Game');
-            document.querySelector('.score').textContent=0;
+            document.querySelector('.message').textContent='😭You lost the Game';
+            displayScore(0);
+        }
+    }
+    //when guess is -ve number
+    else if(guess<0){
+        if(score>1){
+            displayMessage('😒-ve Number!');
+            score--;
+            displayScore(score);
+        }
+        else{
+            displayMessage('😭You lost the Game');
+            displayScore(0);
         }
     }
 
-    // //when guess is too high
-    // else if(guess > secretNumber){
-    //     if(score > 1){
-    //         document.querySelector('.message').textContent='📈Too High!';
-    //         score--;
-    //         document.querySelector('.score').textContent=score;
-    //     }
-    //     else{
-    //         document.querySelector('.message').textContent='😬You Lost the Game';
-    //         document.querySelector('.score').textContent=0;
-    //     }
-    // }
-    // //when guess is too low
-    // else{
-    //     if(score >1){
-    //         document.querySelector('.message').textContent='📉Too Low!';
-    //         score--;
-    //         document.querySelector('.score').textContent=score;
-    //     }
-    //     else{
-    //         document.querySelector('.message').textContent='😬You Lost the Game'
-    //         document.querySelector('.score').textContent=0;
-    //     }
-    // }
+    //When players Wins
+    else if(guess==secretNumber){
+        displayMessage('👍Correct Number!');
+        document.querySelector('.number').textContent=secretNumber;
 
+        document.querySelector('body').style.backgroundColor='#60b347'
+        document.querySelector('.number').style.width='30rem';
+        if(score>highscore){
+            highscore=score;
+            document.querySelector('.highscore').textContent=highscore;
+        }
+
+    }
+    //when guess is not secretNumber
+    else if(guess != secretNumber){
+        if(score>1){
+            displayMessage(guess > secretNumber ? '📈Too High!' : '📉Too Low!');
+            score--;
+            displayScore(score);
+        }else{
+            displayMessage('😭You lost the Game');
+            displayScore(0);
+        }
+    }
 });
 
-
-document.querySelector('.again').addEventListener('click', 
-function(){
+document.querySelector('.again').addEventListener('click',function(){
     score=20;
-    secretNumber=Math.trunc( Math.random() * 20 ) + 1;
-
-    // document.querySelector('.message').textContent='Start guessing rohit....';
-    displayMessage('Start guessing rohit....');
-
-    document.querySelector('.score').textContent=score;
+    secretNumber = Math.trunc(Math.random()*20)+1;
+    displayMessage('😁Start guessing...');
+    displayScore(score);
 
     document.querySelector('.number').textContent='?';
-
-    document.querySelector('.guess').value='';
-    document.querySelector('body').style.backgroundColor='#222';
-    document.querySelector('.number').style.width='15rem'
-
+    
+    //error in line 93
+    document.querySelector('.guess').textContent='';
+    document.querySelector('body').style.backgroundColor='#222'
+    document.querySelector('.number').style.width='15rem';
 });
